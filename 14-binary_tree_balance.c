81333648627d7e5ef3f092ea0008c13dbdccecc2
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-08-01 01:52:39 -0700

    binarytree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,18 +8,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        if (tree != NULL)
-        {
-                size_t left = 0, right = 0;
+	if (tree != NULL)
+	{
+		size_t left = 0, right = 0;
 
-                left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-                right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-                if (left > right)
-                        return (left)
-                return (right);
-        }
-        return (0);
+		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		if (left > right)
+			return (left)
+		return (right);
+	}
+	return (0);
 }
+
 /**
  * binary_tree_balance - function that measures the balance
  * factor of a binary tree
